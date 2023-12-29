e9976b4ec96a79b45ad0205229fc9bf1fe2d706f
DanielDimitrov5 <danieldimitrov2304@gmail.com>
2023-12-29 14:22:33 +0200

    Setup Prisma ORM

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
-import {ethers} from 'ethers';
+import { ethers } from 'ethers';
+import { PrismaClient } from '@prisma/client';
 
-function main() {
+const prisma = new PrismaClient();
+
+
+
+async function main() {
+    const newSig = await prisma.signatures.create({
+        data: {
+            address: "0x1234567890123456789012345678901234567890",
+            signature: "0x000215e1AEbBB1f4DE1b9c5f68C03d5E6f6B39A8"
+        },
+    });
+
+    console.log(newSig);
 }
-main();
+
+main()
+    .catch(e => {
+        throw e
+    })
+    .finally(async () => {
+        await prisma.$disconnect()
+    });
